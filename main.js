d12420b6c96468580d109cfd8568b991fd4658c8
LuciaRyan <lcryan06@gmail.com>
2022-10-31 11:02:25 +0100

    1a. done

--- main.js
+++ main.js
@@ -161,3 +161,22 @@ const inventory = [
     sold: 8,
   },
 ];
+
+/*
+### Opdracht 1 - Array Methoden
+
+* **Opdracht 1a:** Gebruik een array-methode om een array te maken met alle tv-type namen. Log de uitkomst in de
+console.
+* **Opdracht 1b:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht
+zijn. Log de uitkomst in de console.
+* **Opdracht 1c:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight
+beschikken. Log de uitkomst in de console.
+* **Opdracht 1d:** Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert. Log de uitkomst in de
+console.log*/
+
+//1a.//
+const allTvNames = inventory.map((tvName) => {
+return tvName.name;
+});
+
+console.log(allTvNames);
