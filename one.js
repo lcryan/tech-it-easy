4efcb2926923dae8b8652461aaf8b8ccd8c6cda5
LuciaRyan <lcryan06@gmail.com>
2022-11-06 19:51:26 +0100

    bonus material under construction

--- one.js
+++ one.js
@@ -1,9 +1,37 @@
 //1a.//
-const allTvNames = inventory.map((tvName) => {
+/*const allTvNames = inventory.map((tvName) => {
     return tvName.name;
 });
 
-console.log(allTvNames);
+console.log(allTvNames);*/
+
+//writing a function for 1a//
+
+/*function allTvNames(tvArray) {
+    tvArray.map((tvName) =>{
+        let tvNames = 0;
+        tvNames = tvName.name
+        return tvNames
+    })
+    return tvArray
+}
+
+console.log(allTvNames(inventory))*/
+//1a in combo with bonus assignment//
+
+function allTvNames(tvArray) {
+    tvArray.map(({name: tvNames}) =>{
+        return tvNames
+    })
+    return tvArray
+}
+
+console.log(allTvNames(inventory));
+
+const buttonElementOne = document.getElementById('button-one');
+buttonElementOne.addEventListener('click',allTvNames);
+
+
 
 //1b.//
 
